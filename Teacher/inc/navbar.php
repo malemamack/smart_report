13bbefd667e6d31dc3a3ba03e2a579f53aaf67da
malemamack <malemamack@gmail.com>
2024-11-18 15:12:26 +0200

    otp styling

--- Teacher/inc/navbar.php
+++ Teacher/inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="../logo.png" width="40">
+      <img src="../1.jpg" width="40">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
